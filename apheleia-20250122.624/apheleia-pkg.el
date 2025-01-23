c5db22d28536069e253784b87fb9ac77471ce947
John W <j_zhw@proton.me>
2025-01-23 00:29:13 -0600

    add: typescript lang support with formatting

---
+++ apheleia-20250122.624/apheleia-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "apheleia" "20250122.624"
+  "Reformat buffer stably."
+  '((emacs "27"))
+  :url "https://github.com/radian-software/apheleia"
+  :commit "049325e7734603e3c7ba05f26016beccafefb473"
+  :revdesc "049325e77346"
+  :keywords '("tools")
+  :authors '(("Radian LLC" . "contact+apheleia@radian.codes"))
+  :maintainers '(("Radian LLC" . "contact+apheleia@radian.codes")))
